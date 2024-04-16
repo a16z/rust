1682ff613104bb979d6e573361fc74cec0516785
Noah Citron <noah@jeff.org>
2024-04-16 23:17:48 +0100

    use jolt panic handler

--- library/panic_abort/src/zkvm.rs
+++ library/panic_abort/src/zkvm.rs
@@ -1,24 +1,9 @@
-// use alloc::string::String;
 use core::panic::PanicPayload;
 
 pub(crate) unsafe fn zkvm_set_abort_message(_payload: &mut dyn PanicPayload) {
-    // TODO: fix
+    extern "C" {
+        fn jolt_panic() -> !;
+    }
 
-    // let payload = payload.get();
-    // let msg = match payload.downcast_ref::<&'static str>() {
-    //     Some(msg) => msg.as_bytes(),
-    //     None => match payload.downcast_ref::<String>() {
-    //         Some(msg) => msg.as_bytes(),
-    //         None => &[],
-    //     },
-    // };
-    // if msg.is_empty() {
-    //     return;
-    // }
-
-    // extern "C" {
-    //     fn sys_panic(msg_ptr: *const u8, len: usize) -> !;
-    // }
-
-    // sys_panic(msg.as_ptr(), msg.len());
+    jolt_panic()
 }
