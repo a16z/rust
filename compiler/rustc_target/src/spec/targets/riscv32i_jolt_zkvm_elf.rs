f55645c560454939a9e562a00a0ebbc87d7e6373
Noah Citron <noah@jeff.org>
2024-04-16 19:05:18 +0100

    fix allocator

--- compiler/rustc_target/src/spec/targets/riscv32i_jolt_zkvm_elf.rs
+++ compiler/rustc_target/src/spec/targets/riscv32i_jolt_zkvm_elf.rs
@@ -29,6 +29,7 @@ pub fn target() -> Target {
             emit_debug_gdb_scripts: false,
             eh_frame_header: false,
             singlethread: true,
+            supports_stack_protector: false,
             ..Default::default()
         },
     }
--- library/std/src/sys/pal/zkvm/alloc.rs
+++ library/std/src/sys/pal/zkvm/alloc.rs
@@ -1,10 +1,54 @@
-use crate::alloc::{GlobalAlloc, Layout, System};
+use crate::{alloc::{GlobalAlloc, Layout, System}, cell::UnsafeCell};
+
+static mut BUMP_ALLOC: BumpAllocator = BumpAllocator::new();
 
 #[stable(feature = "alloc_system_type", since = "1.28.0")]
 unsafe impl GlobalAlloc for System {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        core::ptr::null_mut()
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        BUMP_ALLOC.alloc(layout)
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
+
+extern "C" {
+    static _HEAP_PTR: u8;
+}
+
+pub struct BumpAllocator {
+    offset: UnsafeCell<usize>,
+}
+
+unsafe impl Sync for BumpAllocator {}
+
+fn heap_start() -> usize {
+    unsafe { _HEAP_PTR as *const u8 as usize }
+}
+
+impl BumpAllocator {
+    pub const fn new() -> Self {
+        Self {
+            offset: UnsafeCell::new(0),
+        }
+    }
+
+    pub fn free_memory(&self) -> usize {
+        heap_start() + (self.offset.get() as usize)
+    }
+}
+
+unsafe impl GlobalAlloc for BumpAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let alloc_start = align_up(self.free_memory(), layout.align());
+        let alloc_end = alloc_start + layout.size();
+        *self.offset.get() = alloc_end - self.free_memory();
+
+        alloc_start as *mut u8
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
+
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
