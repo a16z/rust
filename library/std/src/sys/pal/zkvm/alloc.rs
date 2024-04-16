647f6d356a5844898cec248d18235ab362273432
Noah Citron <noah@jeff.org>
2024-04-16 17:11:38 +0100

    remove sys functions

--- library/std/src/sys/pal/zkvm/alloc.rs
+++ library/std/src/sys/pal/zkvm/alloc.rs
@@ -1,15 +1,10 @@
-use super::abi;
 use crate::alloc::{GlobalAlloc, Layout, System};
 
 #[stable(feature = "alloc_system_type", since = "1.28.0")]
 unsafe impl GlobalAlloc for System {
-    #[inline]
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        abi::sys_alloc_aligned(layout.size(), layout.align())
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        core::ptr::null_mut()
     }
 
-    #[inline]
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        // this allocator never deallocates memory
-    }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
 }
--- library/std/src/sys/pal/zkvm/mod.rs
+++ library/std/src/sys/pal/zkvm/mod.rs
@@ -1,4 +1,4 @@
-//! System bindings for the risc0 zkvm platform
+//! System bindings for the jolt zkvm platform
 //!
 //! This module contains the facade (aka platform-specific) implementations of
 //! OS level functionality for zkvm.
@@ -7,11 +7,10 @@
 //! wide/production use yet, it's still all in the experimental category. This
 //! will likely change over time.
 
-const WORD_SIZE: usize = core::mem::size_of::<u32>();
-
 pub mod alloc;
-#[path = "../zkvm/args.rs"]
+#[path = "../unsupported/args.rs"]
 pub mod args;
+#[path = "../unsupported/env.rs"]
 pub mod env;
 #[path = "../unsupported/fs.rs"]
 pub mod fs;
@@ -19,11 +18,13 @@ pub mod fs;
 pub mod io;
 #[path = "../unsupported/net.rs"]
 pub mod net;
+#[path = "../unsupported/os.rs"]
 pub mod os;
 #[path = "../unsupported/pipe.rs"]
 pub mod pipe;
 #[path = "../unsupported/process.rs"]
 pub mod process;
+#[path = "../unsupported/stdio.rs"]
 pub mod stdio;
 pub mod thread_local_key;
 #[path = "../unsupported/time.rs"]
@@ -71,9 +72,11 @@ pub fn abort_internal() -> ! {
 }
 
 pub fn hashmap_random_keys() -> (u64, u64) {
-    let mut buf = [0u32; 4];
-    unsafe {
-        abi::sys_rand(buf.as_mut_ptr(), 4);
-    };
-    ((buf[0] as u64) << 32 + buf[1] as u64, (buf[2] as u64) << 32 + buf[3] as u64)
+    // let mut buf = [0u32; 4];
+    // unsafe {
+    //     abi::sys_rand(buf.as_mut_ptr(), 4);
+    // };
+    // (buf[0] as u64) << 32 + buf[1] as u64, (buf[2] as u64) << 32 + buf[3] as u64)
+
+    (0, 0)
 }
