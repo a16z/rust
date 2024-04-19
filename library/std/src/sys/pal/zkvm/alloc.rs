3c5f0ec3f4f98a2d211061a83bade8d62c6a6135
Noah Citron <noah@jeff.org>
2024-04-19 20:46:24 +0100

    use externally defined allocator

--- library/std/src/sys/pal/zkvm/alloc.rs
+++ library/std/src/sys/pal/zkvm/alloc.rs
@@ -1,51 +1,11 @@
-use crate::{alloc::{GlobalAlloc, Layout, System}, cell::UnsafeCell};
-use super::abi::_HEAP_PTR;
-
-static mut BUMP_ALLOC: BumpAllocator = BumpAllocator::new();
+use crate::alloc::{GlobalAlloc, Layout, System};
+use super::abi::sys_alloc;
 
 #[stable(feature = "alloc_system_type", since = "1.28.0")]
 unsafe impl GlobalAlloc for System {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        BUMP_ALLOC.alloc(layout)
+        sys_alloc(layout.size(), layout.align())
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
-}
-
-pub struct BumpAllocator {
-    offset: UnsafeCell<usize>,
-}
-
-impl BumpAllocator {
-    pub const fn new() -> Self {
-        Self {
-            offset: UnsafeCell::new(0),
-        }
-    }
-
-    pub fn free_memory(&self) -> usize {
-        heap_start() + (self.offset.get() as usize)
-    }
-}
-
-unsafe impl GlobalAlloc for BumpAllocator {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let alloc_start = align_up(self.free_memory(), layout.align());
-        let alloc_end = alloc_start + layout.size();
-        *self.offset.get() = alloc_end - self.free_memory();
-
-        alloc_start as *mut u8
-    }
-
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
-}
-
-unsafe impl Sync for BumpAllocator {}
-
-fn align_up(addr: usize, align: usize) -> usize {
-    (addr + align - 1) & !(align - 1)
-}
-
-fn heap_start() -> usize {
-    unsafe { _HEAP_PTR as *const u8 as usize }
 }
