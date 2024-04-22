13399cec9e38797a01c6e7f93af3c7d0eb09235d
Noah Citron <noah@jeff.org>
2024-04-22 18:48:12 +0100

    pass message off to panic

--- library/panic_abort/src/zkvm.rs
+++ library/panic_abort/src/zkvm.rs
@@ -1,9 +1,19 @@
 use core::panic::PanicPayload;
+use alloc::string::String;
 
-pub(crate) unsafe fn zkvm_set_abort_message(_payload: &mut dyn PanicPayload) {
-    extern "C" {
-        fn sys_panic() -> !;
-    }
+extern "C" {
+    fn sys_panic(msg_ptr: *const u8, len: usize) -> !;
+}
 
-    sys_panic()
+pub(crate) unsafe fn zkvm_set_abort_message(payload: &mut dyn PanicPayload) {
+    let payload = payload.get();
+    let msg = match payload.downcast_ref::<&'static str>() {
+        Some(msg) => msg.as_bytes(),
+        None => match payload.downcast_ref::<String>() {
+            Some(msg) => msg.as_bytes(),
+            None => &[],
+        },
+    };
+
+    sys_panic(msg.as_ptr(), msg.len())
 }
