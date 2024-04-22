547bfbbe0a69c34b3dc1ec0c6273e8cc1df518f4
Noah Citron <noah@jeff.org>
2024-04-22 12:49:44 +0100

    rename jolt_panic to sys_panic

--- library/panic_abort/src/zkvm.rs
+++ library/panic_abort/src/zkvm.rs
@@ -2,8 +2,8 @@ use core::panic::PanicPayload;
 
 pub(crate) unsafe fn zkvm_set_abort_message(_payload: &mut dyn PanicPayload) {
     extern "C" {
-        fn jolt_panic() -> !;
+        fn sys_panic() -> !;
     }
 
-    jolt_panic()
+    sys_panic()
 }
