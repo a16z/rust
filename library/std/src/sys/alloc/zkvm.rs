7aa15980b10034ce4c9af66d785ae166596259a9
Michael Zhu <mchl.zhu.96@gmail.com>
2024-08-05 14:10:49 -0400

    Fix build

--- library/std/src/sys/alloc/zkvm.rs
+++ library/std/src/sys/alloc/zkvm.rs
@@ -1,10 +1,10 @@
-use crate::alloc::{GlobalAlloc, Layout, System};
 use super::abi::sys_alloc;
+use crate::alloc::{GlobalAlloc, Layout, System};
 
 #[stable(feature = "alloc_system_type", since = "1.28.0")]
 unsafe impl GlobalAlloc for System {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        sys_alloc(layout.size(), layout.align())
+        unsafe { sys_alloc(layout.size(), layout.align()) }
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
